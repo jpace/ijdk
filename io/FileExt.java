0e486156d6a3a933cf0dee1c4c1dddfddfb9857d
Jeff Pace <jeugenepace@gmail.com>
2011-11-03 09:15:49 -0400

    Refined to use StringBuilder instead of StringBuffer.

--- io/FileExt.java
+++ io/FileExt.java
@@ -39,14 +39,13 @@ public class FileExt {
             return null;
         }
         else {
-            StringBuffer buf      = new StringBuffer();
-            String       lineSep  = System.getProperty("line.separator");
+            StringBuilder sb = new StringBuilder();
             
             for (int i = 0; contents != null && i < contents.length; ++i) {
-                buf.append(contents[i] + lineSep);
+                sb.append(contents[i]).append(EOLN);
             }
             
-            return buf.toString();
+            return sb.toString();
         }
     }
 
@@ -56,20 +55,7 @@ public class FileExt {
      * based on return type.
      */
     public static String[] readFile(File file, String[] arg) {
-        try {
-            BufferedReader br    = new BufferedReader(new FileReader(file));
-            List<String>   lines = new ArrayList<String>();
-
-            String in;
-            while ((in = br.readLine()) != null) {
-                lines.add(in);
-            }
-
-            return lines.toArray(new String[lines.size()]);
-        }
-        catch (Exception e) {
-            return null;
-        }
+        return readLines(file);
     }
 
     /**
@@ -106,26 +92,23 @@ public class FileExt {
      * characters (sequences).
      */
     public static String read(FileReader fr, boolean eoln) {
-        String lineSep = eoln ? System.getProperty("line.separator") : null;
-        StringBuffer contents = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         
         try {
             BufferedReader br = new BufferedReader(fr);
 
             String in;
             while ((in = br.readLine()) != null) {
-                // contents.append(in + System.getProperty("line.separator"));
-                contents.append(in);
+                sb.append(in);
                 if (eoln) {
-                    contents.append(lineSep);
+                    sb.append(EOLN);
                 }
             }
 
-            return contents.toString();
+            return sb.toString();
         }
         catch (Exception e) {
             return null;
         }
     }
-
 }
