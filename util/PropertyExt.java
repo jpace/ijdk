c849e5aabc3d4f8fb9935981ae170d25072cf578
Jeff Pace <jeugenepace@gmail.com>
2011-11-06 08:09:04 -0500

    Added property class extensions.

---
+++ util/PropertyExt.java
@@ -0,0 +1,22 @@
+package org.incava.ijdk.util;
+
+import static org.incava.ijdk.util.IUtil.*;
+
+
+public class PropertyExt {
+    /**
+     * Returns the boolean value of the property, or null if it does not exist.
+     */
+    public static Boolean getBooleanProperty(String name) {
+        String val = System.getProperty(name);
+        return isNull(val) ? null : new Boolean(val);
+    }
+
+    /**
+     * Returns the integer value of the property, or null if it does not exist.
+     */
+    public static Integer getIntegerProperty(String name) {
+        String val = System.getProperty(name);
+        return isNull(val) ? null : new Integer(val);
+    }
+}
