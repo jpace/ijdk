38767e5c605a6466e832835bc1fce066dbd21d4c
Jeff Pace <jeugenepace@gmail.com>
2012-01-08 10:00:03 -0500

    Added WriteOptionType, for parameters to the IO output functions.

---
+++ io/WriteOptionType.java
@@ -0,0 +1,13 @@
+package org.incava.ijdk.io;
+
+/**
+ * Options for writing/"printing" files.
+ * 
+ * @see IO#printLines
+ */
+public enum WriteOptionType { 
+    /**
+     * Throw an exception if there is an error writing the file.
+     */
+    WITH_EXCEPTION;
+}
