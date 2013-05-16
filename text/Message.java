5dce776078af857c7d3bb797c7d8a88f3e63cfe3
Jeff Pace <jeugenepace@gmail.com>
2013-05-16 08:52:10 -0400

    Added Message class.

---
+++ text/Message.java
@@ -0,0 +1,19 @@
+package org.incava.idjk.text;
+
+import java.text.MessageFormat;
+
+public class Message {
+    private final String pattern;
+    
+    public Message(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String format(Object ... args) {
+        return MessageFormat.format(pattern, args);
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+}
