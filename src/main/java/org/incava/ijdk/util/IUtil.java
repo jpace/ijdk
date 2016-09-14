50b20fc3fd14c0c741af40fb8a7084fef474f532
Jeff Pace <jeugenepace@gmail.com>
2016-09-13 21:12:35 -0400

    Update comment

--- src/main/java/org/incava/ijdk/util/IUtil.java
+++ src/main/java/org/incava/ijdk/util/IUtil.java
@@ -3,8 +3,8 @@ package org.incava.ijdk.util;
 import org.incava.ijdk.lang.ICore;
 
 /**
- * This class exists for legacy reasons, and its code has been moved to the more accurate ICore, in
- * the IJDK lang package.
+ * This class exists for legacy reasons -- compatibility with older versions of IJDK, and its code
+ * has been moved to the more accurate ICore, in the IJDK lang package.
  */
 public class IUtil extends ICore {
 }
