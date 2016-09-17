d6883176c27c18c3dbfc08dbb47a2a5353b03963
Jeff Pace <jeugenepace@gmail.com>
2016-09-16 22:02:05 -0400

    Add tests for Arrayy

--- src/main/java/ijdk/lang/Arrayy.java
+++ src/main/java/ijdk/lang/Arrayy.java
@@ -3,7 +3,7 @@ package ijdk.lang;
 import java.util.*;
 
 /**
- * Wraps C-style arrays.
+ * Wraps C-style arrays with common behavior.
  */
 public class Arrayy {
     private final Object[] ary;
@@ -12,16 +12,62 @@ public class Arrayy {
         this.ary = ary;
     }
 
+    /**
+     * Returns the wrapped array.
+     */
     public Object[] getArray() {
         return this.ary;
     }
 
+    /**
+     * Returns the wrapped array.
+     */
     public Object[] ary() {
         return this.ary;
     }
 
-    public List<Object> toList() {
-        return null;
+    /**
+     * Returns the wrapped array, as an ArrayList. If the wrapped array is null, then null is
+     * returned.
+     */
+    public ArrayList<Object> asList() {
+        return this.ary == null ? null : new ArrayList<Object>(Arrays.asList(ary));
+    }
+
+    /**
+     * Returns whether all elements in the wrapped array are null. Returns false if the wrapped
+     * array is null.
+     */
+    public boolean areAllNull() {
+        if (this.ary == null) {
+            return false;
+        }
+        else {
+            for (Object obj : ary) {
+                if (obj != null) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    /**
+     * Returns whether any elements in the wrapped array is null. Returns false if the wrapped
+     * array is null.
+     */
+    public boolean isAnyNull() {
+        if (this.ary == null) {
+            return false;
+        }
+        else {
+            for (Object obj : ary) {
+                if (obj == null) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
 
--- src/test/java/ijdk/lang/TestArrayy.java
+++ src/test/java/ijdk/lang/TestArrayy.java
@@ -9,7 +9,93 @@ public class TestArrayy extends TestCase {
         super(name);
     }
 
-    public void testCtor() {
-        Arrayy ary = new Arrayy("this", "is", "a", "test");
+    // ctor
+
+    public Arrayy assertAccessors(Object value, int index, Arrayy ary) {
+        assertEquals(value, ary.getArray()[index]);
+        assertEquals(value, ary.ary()[index]);
+        return ary;
+    }
+
+    public void testCtorNullArray() {
+        Arrayy ary = new Arrayy((Object[])null);
+        assertEquals(null, ary.getArray());
+        assertEquals(null, ary.ary());
+    }
+
+    public void testCtorNullObject() {
+        Arrayy ary = new Arrayy((Object)null);
+        assertAccessors(null, 0, ary);
+    }
+
+    public void testCtorNotEmpty() {
+        Arrayy ary = new Arrayy("a", "b", "c");
+        assertAccessors("a", 0, ary);
+        assertAccessors("b", 1, ary);
+        assertAccessors("c", 2, ary);
+    }
+
+    // asList
+
+    public Arrayy assertAsList(ArrayList<Object> expected, Arrayy ary) {
+        assertEquals(expected, ary.asList());
+        return ary;
+    }
+
+    public void testAsListNullArray() {
+        assertAsList(null, new Arrayy((Object[])null));
+    }
+
+    public void testAsListNullObject() {
+        assertAsList(new ArrayList<Object>(Arrays.asList(new Object[] { null })), new Arrayy((Object)null));
+    }
+
+    public void testAsListNotEmpty() {
+        Arrayy ary = new Arrayy("a", "b", "c");
+        assertAsList(new ArrayList<Object>(Arrays.asList(new Object[] { "a", "b", "c" })), ary);
+    }
+
+    // areAllNull
+
+    public Arrayy assertAreAllNull(boolean expected, Arrayy ary) {
+        assertEquals(expected, ary.areAllNull());
+        return ary;
+    }
+
+    public void testAreAllNullNullArray() {
+        assertAreAllNull(false, new Arrayy((Object[])null));
+    }
+
+    public void testAreAllNullNullObject() {
+        assertAreAllNull(true, new Arrayy((Object)null));
+    }
+
+    public void testAreAllNullNotEmpty() {
+        Arrayy ary = new Arrayy("a", "b", "c");
+        assertAreAllNull(false, ary);
+    }
+
+    // areAllNull
+
+    public Arrayy assertIsAnyNull(boolean expected, Arrayy ary) {
+        assertEquals(expected, ary.isAnyNull());
+        return ary;
+    }
+
+    public void testIsAnyNullNullArray() {
+        assertIsAnyNull(false, new Arrayy((Object[])null));
+    }
+
+    public void testIsAnyNullOnlyNullObject() {
+        assertIsAnyNull(true, new Arrayy((Object)null));
+    }
+
+    public void testIsAnyNullOneNullObject() {
+        assertIsAnyNull(true, new Arrayy("a", null));
+    }
+
+    public void testIsAnyNullNotEmpty() {
+        Arrayy ary = new Arrayy("a", "b", "c");
+        assertIsAnyNull(false, ary);
     }
 }
