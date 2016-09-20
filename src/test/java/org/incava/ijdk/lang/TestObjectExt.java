5f7d7bba3444f18212a0c9b4deae3b6eca3fe8df
Jeff Pace <jeugenepace@gmail.com>
2016-09-19 22:01:13 -0400

    Add all ObjectExt tests

--- src/test/java/org/incava/ijdk/lang/TestObjectExt.java
+++ src/test/java/org/incava/ijdk/lang/TestObjectExt.java
@@ -1,6 +1,5 @@
 package org.incava.ijdk.lang;
 
-import java.io.*;
 import java.util.*;
 import junit.framework.TestCase;
 
@@ -114,5 +113,157 @@ public class TestObjectExt extends TestCase {
 
     public void testCompareGreaterObject() {
         assertCompare(3, new String("def"), new String("abc"));
+    }
+
+    // isTrue
+
+    public boolean assertIsTrue(boolean expected, Object obj) {
+        boolean result = ObjectExt.isTrue(obj);
+        assertEquals("obj: " + obj, expected, result);
+        return result;
+    }
+
+    public void testIsTrueNull() {
+        assertIsTrue(false, null);
+    }
+
+    public void testIsTrueObject() {
+        assertIsTrue(true, new Object());
+    }
+
+    public void testIsTrueEmptyString() {
+        assertIsTrue(false, "");
+    }
+
+    public void testIsTrueNotEmptyString() {
+        assertIsTrue(true, "abc");
+    }
+
+    public void testIsTrueEmptyCollection() {
+        assertIsTrue(false, new ArrayList<String>());
+    }
+
+    public void testIsTrueNotEmptyCollection() {
+        assertIsTrue(true, Arrays.asList(new Integer[] { 1 }));
+    }
+
+    public void testIsTrueEmptyArray() {
+        assertIsTrue(false, new Integer[0]);
+    }
+
+    public void testIsTrueNotEmptyArray() {
+        assertIsTrue(true, new Integer[] { 1 });
+    }
+
+    // isFalse
+
+    public boolean assertIsFalse(boolean expected, Object obj) {
+        boolean result = ObjectExt.isFalse(obj);
+        assertEquals("obj: " + obj, expected, result);
+        return result;
+    }
+
+    public void testIsFalseNull() {
+        assertIsFalse(true, null);
+    }
+
+    public void testIsFalseObject() {
+        assertIsFalse(false, new Object());
+    }
+
+    public void testIsFalseEmptyString() {
+        assertIsFalse(true, "");
+    }
+
+    public void testIsFalseNotEmptyString() {
+        assertIsFalse(false, "abc");
+    }
+
+    public void testIsFalseEmptyCollection() {
+        assertIsFalse(true, new ArrayList<String>());
+    }
+
+    public void testIsFalseNotEmptyCollection() {
+        assertIsFalse(false, Arrays.asList(new Integer[] { 1 }));
+    }
+
+    public void testIsFalseEmptyArray() {
+        assertIsFalse(true, new Integer[0]);
+    }
+
+    public void testIsFalseNotEmptyArray() {
+        assertIsFalse(false, new Integer[] { 1 });
+    }    
+
+    // isEmpty
+
+    public boolean assertIsEmpty(boolean expected, Object obj) {
+        boolean result = ObjectExt.isEmpty(obj);
+        assertEquals("obj: " + obj, expected, result);
+        return result;
+    }
+
+    public void testIsEmptyNull() {
+        assertIsEmpty(true, null);
+    }
+
+    public void testIsEmptyObject() {
+        assertIsEmpty(false, new Object());
+    }
+
+    public void testIsEmptyEmptyString() {
+        assertIsEmpty(true, "");
+    }
+
+    public void testIsEmptyNotEmptyString() {
+        assertIsEmpty(false, "abc");
+    }
+
+    public void testIsEmptyEmptyCollection() {
+        assertIsEmpty(true, new ArrayList<String>());
+    }
+
+    public void testIsEmptyNotEmptyCollection() {
+        assertIsEmpty(false, Arrays.asList(new Integer[] { 1 }));
+    }
+
+    public void testIsEmptyEmptyArray() {
+        assertIsEmpty(true, new Integer[0]);
+    }
+
+    public void testIsEmptyNotEmptyArray() {
+        assertIsEmpty(false, new Integer[] { 1 });
+    }
+
+    // isNull
+
+    public boolean assertIsNull(boolean expected, Object obj) {
+        boolean result = ObjectExt.isNull(obj);
+        assertEquals("obj: " + obj, expected, result);
+        return result;
+    }
+
+    public void testIsNullNull() {
+        assertIsNull(true, null);
+    }
+
+    public void testIsNullObject() {
+        assertIsNull(false, new Object());
+    }
+
+    // isNotNull
+
+    public boolean assertIsNotNull(boolean expected, Object obj) {
+        boolean result = ObjectExt.isNotNull(obj);
+        assertEquals("obj: " + obj, expected, result);
+        return result;
+    }
+
+    public void testIsNotNullNull() {
+        assertIsNotNull(false, null);
+    }
+
+    public void testIsNotNullObject() {
+        assertIsNotNull(true, new Object());
     }
 }
