5c544e586340a8e81c808ad2e2a3a59a56dd226e
Jeff Pace <jeugenepace@gmail.com>
2016-09-25 11:05:34 -0400

    Add Output class

---
+++ src/test/java/ijdk/lang/TestOutput.java
@@ -0,0 +1,13 @@
+package ijdk.lang;
+
+import junit.framework.TestCase;
+
+public class TestOutput extends TestCase {
+    public TestOutput(String name) {
+        super(name);
+    }
+
+    public void testNothing() {
+        
+    }
+}
