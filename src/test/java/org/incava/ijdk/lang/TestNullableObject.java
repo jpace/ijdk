e9c16123c05dd2c284817d59545a2eabce86aeda
Jeff Pace <jeugenepace@gmail.com>
2017-06-03 12:01:20 -0400

    Parameterize tests for toString and get methods of Obj class.

--- src/test/java/org/incava/ijdk/lang/TestNullableObject.java
+++ src/test/java/org/incava/ijdk/lang/TestNullableObject.java
@@ -1,27 +1,47 @@
 package org.incava.ijdk.lang;
 
-import org.incava.ijdk.lang.ObjectTest;
-import org.junit.Assert;
+import java.util.Collection;
+import java.util.List;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.incava.test.Assertions.assertEqual;
+import static org.incava.test.Assertions.assertSame;
 import static org.incava.test.Assertions.message;
 import static org.incava.test.Parameters.params;
 import static org.incava.test.Parameters.paramsList;
 
+@RunWith(JUnitParamsRunner.class)
 public class TestNullableObject {
     @Test
-    public void toStringString() {
-        assertEqual("one", NullableObject.of("one").toString());
+    @Parameters
+    @TestCaseName("{method} {index} {params}")
+    public void toStringTest(String expected, Object x) {
+        NullableObject<Object> obj = NullableObject.of(x);
+        String result = obj.toString();
+        assertEqual(expected, result, message("x", x));
+    }
+    
+    private List<Object[]> parametersForToStringTest() {
+        return paramsList(params("one", "one"),
+                          params("null", null),
+                          params("[a, b, c]", new Object[] { "a", "b", "c" }));
     }
 
     @Test
-    public void toStringNull() {
-        assertEqual("null", NullableObject.of(null).toString());
+    @Parameters
+    @TestCaseName("{index} {method} {params}")
+    public void get(Object expected, Object fromObj) {
+        NullableObject<Object> obj = NullableObject.of(fromObj);
+        Object result = obj.get();
+        assertSame(expected, result, message("fromObj", fromObj));
+    }
+    
+    private List<Object[]> parametersForGet() {
+        return paramsList(params("abc", "abc"));
     }
 
-    @Test
-    public void toStringArray() {
-        assertEqual("[a, b, c]", NullableObject.of(new Object[] { "a", "b", "c" }).toString());
-    }    
 }
