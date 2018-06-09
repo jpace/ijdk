ccc00716e556cbc18b5e38cd8d4fb50e4df0af78
Jeff Pace <jeugenepace@gmail.com>
2018-06-09 12:03:35 -0400

    Refine iteration between two numbers, in Iterate#count and Range iterator.

---
+++ src/main/java/org/incava/ijdk/collect/IntegerIterator.java
@@ -0,0 +1,43 @@
+package org.incava.ijdk.collect;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.incava.ijdk.collect.Array;
+
+/**
+ * An iterator from one number to another, inclusive, using a step, which defaults to 1.
+ */
+public class IntegerIterator implements Iterator<Integer> {
+    private Integer current;
+    private final Integer last;
+    private final Integer step;
+        
+    public IntegerIterator(Integer first, Integer last) {
+        this(first, last, 1);
+    }
+
+    public IntegerIterator(Integer first, Integer last, Integer step) {
+        this.current = first;
+        this.last = last;
+        this.step = step;
+    }
+
+    public Integer next() {
+        if (hasNext()) {
+            Integer value = this.current;
+            this.current += step;
+            return value;
+        }
+        else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    public boolean hasNext() {
+        return this.current.intValue() <= this.last.intValue();
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    } 
+}
---
+++ src/test/java/org/incava/ijdk/collect/IntegerIteratorTest.java
@@ -0,0 +1,73 @@
+package org.incava.ijdk.collect;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
+import org.incava.attest.Parameterized;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class IntegerIteratorTest extends Parameterized {
+    @Test @Parameters @TestCaseName("{method} {index} {params}")
+    public void hasNext(boolean expected, IntegerIterator rit) {
+        assertThat(rit.hasNext(), equalTo(expected));
+    }
+    
+    private List<Object[]> parametersForHasNext() {
+        return paramsList(params(true,  new IntegerIterator(0, 1)),
+                          params(true,  new IntegerIterator(1, 2)),
+                          params(true,  new IntegerIterator(1, 1)),
+                          params(false, new IntegerIterator(2, 1)),
+                          params(true,  new IntegerIterator(0, 1, 1)),
+                          params(true,  new IntegerIterator(0, 1, 2)),
+                          params(true,  new IntegerIterator(1, 1, 1)),
+                          params(false, new IntegerIterator(2, 1, 1)));
+    }
+
+    @Test @Parameters @TestCaseName("{method} {index} {params}")
+    public void next(Integer expected, IntegerIterator rit) {
+        if (expected == null) {
+            try {
+                rit.next();
+                throw new RuntimeException("expected exception");
+            }
+            catch (NoSuchElementException nsee) {
+                // good
+            }
+        }
+        else {
+            assertThat(rit.next(), equalTo(expected));
+        }
+    }
+    
+    private List<Object[]> parametersForNext() {
+        IntegerIterator x = new IntegerIterator(1, 2, 1);
+        x.next();
+        IntegerIterator y = new IntegerIterator(1, 2, 2);
+        y.next();
+        return paramsList(params(0,    new IntegerIterator(0, 1)),
+                          params(1,    new IntegerIterator(1, 2)),
+                          params(1,    new IntegerIterator(1, 1)),
+                          params(1,    new IntegerIterator(1, 1, 1)),
+                          params(1,    new IntegerIterator(1, 1, 2)),
+                          params(2,    x),
+                          params(null, y),
+                          params(null, new IntegerIterator(2, 1)));
+    }    
+
+    @Test
+    public void remove() {
+        IntegerIterator rit = new IntegerIterator(0, 1);
+        try {
+            rit.remove();
+            throw new RuntimeException("expected exception");
+        }
+        catch (UnsupportedOperationException uoe) {
+            // good
+        }
+    }
+}
