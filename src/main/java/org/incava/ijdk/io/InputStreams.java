0daa2c384e2d08776bf5d74e03ecbc140727cf82
Jeff Pace <jpace@softwareag.com>
2018-06-10 13:55:28 -0400

    Read StringArray from InputStream.

--- src/main/java/org/incava/ijdk/io/InputStreams.java
+++ src/main/java/org/incava/ijdk/io/InputStreams.java
@@ -1,10 +1,12 @@
 package org.incava.ijdk.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.EnumSet;
+import java.util.List;
 
 public class InputStreams {
     /**
@@ -28,5 +30,33 @@ public class InputStreams {
      */
     public static List<String> readLines(InputStream stream, EnumSet<ReadOptionType> options) throws IORuntimeException {
         return Readers.readLines(new InputStreamReader(stream), options);
+    }
+
+    /**
+     * Reads lines from an input stream into a list of strings.
+     */
+    public static void readLines(List<String> list, InputStream stream) {
+        InputStreamReader isr = null;
+        try {
+            isr = new InputStreamReader(stream);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+        }
+        catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+        finally {
+            try {
+                if (isr != null) {
+                    isr.close();
+                }
+            }
+            catch (IOException ioe) {
+                throw new RuntimeException(ioe);
+            }
+        }
     }
 }
